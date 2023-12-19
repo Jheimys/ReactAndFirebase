8b8d17ae12eb7f817edc7d68ed551675299f2f31
Jheimys Bassani <jheimysbb@gmail.com>
2023-12-19 20:30:26 +0000

    :construction:Updating the Navibar component

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -6,6 +6,8 @@ import { auth } from "../../firebase/firebaseConection";
 
 import "./Navbar.css";
 
+import logo from "../../img/reactflix30.png";
+
 const Navbar = () => {
   const { user, loadingUser } = useAuth();
   const navigate = useNavigate();
@@ -27,48 +29,58 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <span>ReactFlix</span>
-      <ul>
-        {!user && (
-          <>
-            <li>
-              <Link to="/">Login</Link>
-            </li>
-          </>
-        )}
+      <div className="logo">
+        <img
+          className="img-logo"
+          src={logo}
+          alt="Logo do reactflix"
+          onClick={() => navigate("/home")}
+        />
+      </div>
 
-        {!user && (
-          <>
-            <li>
-              <Link to="/cadastro">Cadastrar</Link>
-            </li>
-          </>
-        )}
+      <div className="container-lis">
+        <ul>
+          {!user && (
+            <>
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+            </>
+          )}
 
-        {user && (
-          <>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-          </>
-        )}
+          {!user && (
+            <>
+              <li>
+                <Link to="/cadastro">Cadastrar</Link>
+              </li>
+            </>
+          )}
 
-        {user && (
-          <>
-            <li>
-              <Link to="/conta">Minha conta</Link>
-            </li>
-          </>
-        )}
+          {user && (
+            <>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+            </>
+          )}
 
-        {user && (
-          <>
-            <li>
-              <button onClick={handleLogout}>Sair</button>
-            </li>
-          </>
-        )}
-      </ul>
+          {user && (
+            <>
+              <li>
+                <Link to="/conta">Minha conta</Link>
+              </li>
+            </>
+          )}
+
+          {user && (
+            <>
+              <li>
+                <button onClick={handleLogout}>Sair</button>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
